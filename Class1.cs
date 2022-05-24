66649ef9ce3f4079c0f639ba768b3b3bcad2552d
Alexzandrea M Olson <98174336+Alexzandrea-Olson@users.noreply.github.com>
2022-05-24 02:03:28 -0500

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Class1
+    {
+        public string bookTitle;
+        public string bookAuthor;
+        public int bookPages;
+
+        //empty constructor
+        public Class1()
+        {
+
+        }
+
+        //3 perameter constructor
+        public Class1(string title, string author, int numPages)
+        {
+            this.bookTitle = title;
+            this.bookAuthor = author;
+            this.bookPages = numPages;
+        }
+
+        public void setBookTitle(string bookTitle)
+         {
+             this.bookTitle = bookTitle; 
+        }
+
+        public void setBookAuthor(string author)
+        {
+           this.bookAuthor = author;
+        }
+
+        public void setBookPages(int numPages)
+        {
+            this.bookPages = numPages;
+        }
+
+        public String getBookTitle()
+        {
+            return bookTitle;
+        }
+
+        public String getBookAuthor()
+        {
+            return bookAuthor;
+        }
+
+        public int getBookPages()
+        {
+            return bookPages;
+        }
+
+        //printing all book information 
+        public void toString()
+        {
+            Console.WriteLine(getBookTitle());
+            Console.WriteLine(getBookAuthor());
+            Console.WriteLine(getBookPages());
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //creating object from class one with no constructor
+            Class1 book1 = new Class1();
+
+            //multi line input vs utalizing a constructor
+            book1.bookTitle = "Harry Potter";
+            book1.bookAuthor = "JK Rowling";
+            book1.bookPages = 233;
+            
+            //multile line displaying to user
+            Console.WriteLine(book1.bookTitle);
+            Console.WriteLine(book1.bookAuthor);
+            Console.WriteLine(book1.bookPages);
+            Console.WriteLine();
+
+
+            //creating object from class with 3 peramater constructor
+            Class1 book2 = new Class1("The Hobbit", "J.R.R Tolkien", 304);
+
+            //single line displaying to user
+            book2.toString();
+        }
+    }
+}
